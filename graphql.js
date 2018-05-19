d65ee115a4d100ab9cb96d8917568cd3de0c9154
jimliu7434 <jimliu7434@gmail.com>
2018-05-19 22:23:45 +0800

    graphql 基本實作完成

--- graphql.js
+++ graphql.js
@@ -1,24 +1,14 @@
 const Router = require('koa-router');
-const { graphqlKoa } = require('apollo-server-koa');
+const { graphqlKoa, graphiqlKoa, } = require('apollo-server-koa');
 const router = new Router();
 
 // GraphQL
 const schema1 = require('./scheme1');
 const schema2 = require('./scheme2');
-//const { graphql } = require('graphql');
 
 router.post('/schema1', graphqlKoa({ schema: schema1 }));
 router.post('/schema2', graphqlKoa({ schema: schema2 }));
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql/schema2' }));
 
-//router.post('/', ctx => {
-    // graphql(schema, ctx.request.body)
-    //     .then((result) => {
-    //         ctx.body = JSON.stringify(result, null, 2);
-    //         ctx.status = 200;
-    //     })
-    //     .catch((error) => {
-    //         ctx.status = 500;
-    //     });
-//});
 
 module.exports = router;
--- scheme2.js
+++ scheme2.js
@@ -1,11 +1,65 @@
-const { buildSchema } = require('graphql');
+const graphql = require('graphql');
 
+const {
+    buildSchema,
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLID,
+    GraphQLList,
+    GraphQLNonNull,
+    isOutputType, } = graphql;
+const { makeExecutableSchema } = require('graphql-tools');
 
-let schema = buildSchema(`
-    type Query {
-        postTitle: String,
-        newsTitle: String
+// Some fake data
+const ReservationList = [
+    {
+        time: '20180101',
+        place: 'taipei',
+        user: {
+            name: 'jim',
+            gender: 'male',
+        },
+    },
+    {
+        time: '20180102',
+        place: 'taichung',
+        user: {
+            name: 'jim',
+            gender: 'male',
+        },
+    },
+];
+
+let typeDefs = `
+    type User {
+        name: String
+        gender: String
     }
-`);
+
+    type Reservation {
+        time: String!
+        place: String!
+        user: User!
+    }
+
+    # Must have Query
+    type Query {
+        reservations(time:String!): [Reservation]
+    }
+`;
+
+let resolvers = {
+    Query: {
+        reservations: (_, { time }) => {
+            return ReservationList.filter(r => r.time <= time);
+        }
+    },
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers, });
+
+
+
 
 module.exports = schema;
